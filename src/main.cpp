5da582dc5bd77c9afe591958450fa9c95e6bb9f3
Yicheng Han <donnyhan@student.ubc.ca>
2022-08-06 17:31:50 -0700

    8.6

--- src/main.cpp
+++ src/main.cpp
@@ -15,43 +15,63 @@ Adafruit_SSD1306 display_handler(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET)
 void handle_R_interrupt();
 
 Encoders encoders1 = Encoders();
+int countR = 0;
+int value = 0;
+volatile int interrupt_counter = 0;
+
+void interrupt_handler_main();
 
 void setup() {
-  pinSetup();
- 
-  attachInterrupt(digitalPinToInterrupt(enc_R), handle_R_interrupt, FALLING);
+
+pinMode(PB4,INPUT);
+
+  attachInterrupt(digitalPinToInterrupt(PB4), handle_R_interrupt, RISING);
 
   display_handler.begin(SSD1306_SWITCHCAPVCC, 0x3C);
   display_handler.clearDisplay();
   display_handler.setTextSize(1);
   display_handler.setTextColor(SSD1306_WHITE);
   display_handler.setCursor(0,0);
+  display_handler.println("Starting up...");
   display_handler.display();
 
-  delay(500);
+  delay(1000);
   
-  pwm_start(MOTOR_L_F, PWMFREQ, FWD_SPEED, RESOLUTION_10B_COMPARE_FORMAT);
-  pwm_start(MOTOR_R_F, PWMFREQ, FWD_SPEED, RESOLUTION_10B_COMPARE_FORMAT);
+  // pwm_start(MOTOR_L_F, PWMFREQ, FWD_SPEED, RESOLUTION_10B_COMPARE_FORMAT);
+  // pwm_start(MOTOR_R_F, PWMFREQ, FWD_SPEED, RESOLUTION_10B_COMPARE_FORMAT);
 
 }
 
 void loop(){
-  int countR = 0;
-  int value = 0;
+ 
   display_handler.clearDisplay();
   display_handler.setCursor(0,0);
 
-  countR = encoders1.countR;
+  // countR = encoders1.countR;
 
-  display_handler.println("countR:");
-  display_handler.println(countR,DEC);
+  display_handler.print("Counter:");
+  display_handler.println(interrupt_counter);
 
+  display_handler.print("CountR:");
+  display_handler.println(encoders1.countR);
+
+  // value++;
+  // if (value > 1000) {
+  //   value = 0;
+  // }
+  // display_handler.println(value);
   display_handler.display();
-  delay(200);
+  delay(20);
   
+  }
+
+  void interrupt_handler_main()
+  {
+    
   }
 
 void handle_R_interrupt()
 {
-  encoders1.handle_R_interrupt();
+  interrupt_counter++;
+encoders1.handle_R_interrupt();
 }
